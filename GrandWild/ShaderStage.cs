43f57abc95510bc8a5d6619024dde10cf2bbd88e
wzx1996 <wzx1996@gmail.com>
2017-01-21 03:33:19 +0800

    ShaderStage module summary and copyright

--- GrandWild/ShaderStage.cs
+++ GrandWild/ShaderStage.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿///<summary>
+///A class for loading a SPIR-V shader as a module and define a shader stage of that module.
+///</summary>
+/// 
+/// Copyright (C) FlameRat 2016-2017
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +26,7 @@ using System.IO;
 using Vulkan;
 
 namespace org.flamerat.GrandWild {
+
     class ShaderStage {
         public PipelineShaderStageCreateInfo Info { get; protected set; }
 
