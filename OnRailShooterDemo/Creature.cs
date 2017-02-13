b8e6e08d64712d78d012b412dc492b9211a53468
FlameRat <wzx1996@gmail.com>
2017-02-13 23:44:38 +0800

    start developing creature classes

---
+++ OnRailShooterDemo/Creature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.flamerat.GrandWild.Entity;
+
+namespace org.flamerat.OnRailShooterDemo {
+    class Creature:TimerBasedEntity {
+    }
+}
