c870b6de3cbc78fa5f05010c9f5455e3556682b3
FlameRat <wzx1996@gmail.com>
2017-02-13 23:33:10 +0800

    let some previously protected member of Structure (and derived classes) to be public or private

--- GrandWild/Entity/Entity.cs
+++ GrandWild/Entity/Entity.cs
@@ -12,21 +12,21 @@ namespace org.flamerat.GrandWild.Entity {
         public delegate void OnMoveEvent(Entity sender, vec3 oldPosition, vec3 newPosition);
         public event OnMoveEvent OnMove;
         public void MoveTo(vec3 position) {
-            var oldPosition = _Position;
-            _Position = position;
-            OnMove(this,oldPosition,_Position);
+            var oldPosition = Position;
+            Position = position;
+            OnMove(this,oldPosition,Position);
         }
         public void MoveFor(vec3 relativePosition) {
-            var oldPosition = _Position;
-            _Position += relativePosition;
-            OnMove(this, oldPosition, _Position);
+            var oldPosition = Position;
+            Position += relativePosition;
+            OnMove(this, oldPosition, Position);
         }
         public void MoveForSelfCoordinate(float forward,float right,float up) {
             var selfMovement = new vec4(right, up, -forward, 1);
             mat4 rotateMatrix = new mat4(1);
-            glm.rotate(rotateMatrix, glm.radians(_XRotation), new vec3(1, 0, 0));
-            glm.rotate(rotateMatrix, glm.radians(_YRotation), new vec3(0, 1, 0));
-            glm.rotate(rotateMatrix, glm.radians(_ZRotation), new vec3(0, 0, 1));
+            glm.rotate(rotateMatrix, glm.radians(XRotation), new vec3(1, 0, 0));
+            glm.rotate(rotateMatrix, glm.radians(YRotation), new vec3(0, 1, 0));
+            glm.rotate(rotateMatrix, glm.radians(ZRotation), new vec3(0, 0, 1));
             selfMovement = rotateMatrix * selfMovement;
             MoveFor(new vec3(selfMovement));
         }
@@ -34,34 +34,34 @@ namespace org.flamerat.GrandWild.Entity {
         public delegate void OnRotateEvent(Entity sender, float oldXR, float oldYR, float oldZR,float newXR,float newYR,float newZR);
         public event OnRotateEvent OnRotate;
         public void RotateTo(float xr,float yr,float zr) {
-            var oldXR = _XRotation;
-            var oldYR = _YRotation;
-            var oldZR = _ZRotation;
-            _XRotation = xr;
-            _YRotation = yr;
-            _ZRotation = zr;
-            OnRotate(this, oldXR, oldYR, oldZR, _XRotation, _YRotation, _ZRotation);
-            if (_XRotation > 180) _XRotation -= 360;
-            if (_XRotation > 180) _YRotation -= 360;
-            if (_XRotation > 180) _ZRotation -= 360;
-            if (_XRotation < -180) _XRotation += 360;
-            if (_XRotation < -180) _YRotation += 360;
-            if (_XRotation < -180) _ZRotation += 360;
+            var oldXR = XRotation;
+            var oldYR = YRotation;
+            var oldZR = ZRotation;
+            XRotation = xr;
+            YRotation = yr;
+            ZRotation = zr;
+            OnRotate(this, oldXR, oldYR, oldZR, XRotation, YRotation, ZRotation);
+            if (XRotation > 180) XRotation -= 360;
+            if (XRotation > 180) YRotation -= 360;
+            if (XRotation > 180) ZRotation -= 360;
+            if (XRotation < -180) XRotation += 360;
+            if (XRotation < -180) YRotation += 360;
+            if (XRotation < -180) ZRotation += 360;
         }
         public void RotateFor(float xr,float yr,float zr) {
-            var oldXR = _XRotation;
-            var oldYR = _YRotation;
-            var oldZR = _ZRotation;
-            _XRotation += xr;
-            _YRotation += yr;
-            _ZRotation += zr;
-            OnRotate(this, oldXR, oldYR, oldZR, _XRotation, _YRotation, _ZRotation);
-            if(_XRotation > 180) _XRotation -= 360;
-            if (_XRotation > 180) _YRotation -= 360;
-            if (_XRotation > 180) _ZRotation -= 360;
-            if (_XRotation < -180) _XRotation += 360;
-            if (_XRotation < -180) _YRotation += 360;
-            if (_XRotation < -180) _ZRotation += 360;
+            var oldXR = XRotation;
+            var oldYR = YRotation;
+            var oldZR = ZRotation;
+            XRotation += xr;
+            YRotation += yr;
+            ZRotation += zr;
+            OnRotate(this, oldXR, oldYR, oldZR, XRotation, YRotation, ZRotation);
+            if(XRotation > 180) XRotation -= 360;
+            if (XRotation > 180) YRotation -= 360;
+            if (XRotation > 180) ZRotation -= 360;
+            if (XRotation < -180) XRotation += 360;
+            if (XRotation < -180) YRotation += 360;
+            if (XRotation < -180) ZRotation += 360;
         }
         public void RotateForSelfCoordinate(float pan,float pitch,float tilt) {
             throw new NotImplementedException();
@@ -91,25 +91,21 @@ namespace org.flamerat.GrandWild.Entity {
             OnDestroy(this);
         }
 
-        protected vec3 _Position;
-        protected float _Scale;
-        protected float _XRotation;
-        protected float _YRotation;
-        protected float _ZRotation;
-        protected mat4 _ModelMatrix {
+        public vec3 Position { get; private set; }
+        public float Scale { get; private set; }
+        public float XRotation { get; private set; }
+        public float YRotation { get; private set; }
+        public float ZRotation { get; private set; }
+        private mat4 _ModelMatrix {
             get {
                 mat4 result=new mat4(1);
-                glm.scale(result, new vec3(_Scale));
-                glm.rotate(result, glm.radians(_XRotation), new vec3(1, 0, 0));
-                glm.rotate(result, glm.radians(_YRotation), new vec3(0, 1, 0));
-                glm.rotate(result, glm.radians(_ZRotation), new vec3(0, 0, 1));
-                glm.translate(result, _Position);
+                glm.scale(result, new vec3(Scale));
+                glm.rotate(result, glm.radians(XRotation), new vec3(1, 0, 0));
+                glm.rotate(result, glm.radians(YRotation), new vec3(0, 1, 0));
+                glm.rotate(result, glm.radians(ZRotation), new vec3(0, 0, 1));
+                glm.translate(result, Position);
                 return result;
             }
-        }
-        protected new SubPart[] _SubParts {
-            get { return base._SubParts; }
-            set { base._SubParts = value; }
         }
 
     }
